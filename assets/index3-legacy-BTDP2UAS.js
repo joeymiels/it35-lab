75f61015de97b272d01a2e08847fdb8a4bbb07a3
joeymiels <20211595@nbsc.edu.ph>
2025-04-02 21:48:50 +0800

    Updates

--- assets/index3-legacy-BTDP2UAS.js
+++ assets/index3-legacy-BTDP2UAS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DiyaM72s.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
+System.register(["./index-legacy-CxFj30Sb.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-Bm1KWXcc.js
+++ assets/swipe-back-legacy-Bm1KWXcc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DiyaM72s.js","./index3-legacy-DmOg8wmE.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CxFj30Sb.js","./index3-legacy-BTDP2UAS.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
