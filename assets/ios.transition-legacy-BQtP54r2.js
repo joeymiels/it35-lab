ae14ea46ac7e9736f2b22da8a475c72d755705bc
joeymiels <20211595@nbsc.edu.ph>
2025-05-29 13:35:17 +0800

    Updates

--- assets/ios.transition-legacy-BQtP54r2.js
+++ assets/ios.transition-legacy-BQtP54r2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CpU6-VDu.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BI2mQY7H.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DU6jjD7Z.js
+++ assets/swipe-back-legacy-DU6jjD7Z.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CpU6-VDu.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-BI2mQY7H.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
